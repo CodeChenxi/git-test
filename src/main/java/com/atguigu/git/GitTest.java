c2f83c8652986d386a2d5d8167711f3a2027fe28
linchenxi <linchenxi@cloudkaifa.com>
2022-12-18 12:43:25 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello world!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
+
 
     }
 }
